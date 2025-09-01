366bd8262756447737325ec4c316d1856535da55
폴리나 <elfish33@gmail.com>
2025-09-01 09:51:02 +0900

    php file

---
+++ figma_layout/samlip/footer.php
@@ -0,0 +1,46 @@
+<footer>
+            <!-- top -->
+            <div class="ft_top">
+                <div class="info">
+                <a href="#">개인정보처리방침</a>
+                <a href="#">영상정보처리기기 운영관리방침</a>
+                <a href="#">거래희망회사 사전등록</a>
+                <a href="#">동반성장</a>
+                <a href="#"><img src="./images/footer_001.png" alt="소비자 중심 CCM"></a>
+                </div>
+                
+                <ul class="site_all">
+                    <li>
+                        <p>FAMILY SITES</p> <!-- 화살표는 bg -->
+                        <ul class="family_list">
+                            <li><a href="#">PARIS BAGUETTE</a></li>
+                            <li><a href="#">BASKIN ROBINS</a></li>
+                            <li><a href="#">DUNKIN’</a></li>
+                            <li><a href="#">PARIS CROISSANT</a></li>
+                            <li><a href="#">SHAKE SHAKE</a></li>
+                            <li><a href="#">CAFE PASCUCCI</a></li>
+                            <li><a href="#">JAMBA JUICE</a></li>
+                            <li><a href="#">HAPPY POINT</a></li>
+                            <li><a href="#">LAGRILLA</a></li>
+                            <li><a href="#">PASSION 5</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p>SITE MAP</p>
+                        <div class="sitemap_wrap"><!-- 배경 -->
+                            <div class="sitemap_contents">
+                                <h2>Site Map</h2>
+                                <button type="button" class="close">X</button>
+                                <!-- site map list -->
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <!-- bottom -->
+            <div class="ft_btm">
+                <address>고객센터  전화번호 080-739-8572 (월~금 09:00~17:00)<br>
+                    101, Gongdan 1-daero, Siheung-si, Gyeonggi-do, Republic of Korea ㈜SPC삼립 대표이사 황종현</address>
+                    <p>&copy; SPC Samlip. All rights reserved.</p>
+            </div>
+        </footer>
